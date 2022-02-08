4c97892528bbe54d2f0ed163cf0f51456a446918
Roland Li <r@mmsg.com>
2022-02-07 20:17:35 -0700

    Revert "password recovery template"
    This reverts commit 4a35bd0ffd9e8c3241b6de3f10fcdc4dd0d677e9.

--- objects/userRecoverPass.php
+++ objects/userRecoverPass.php
@@ -30,11 +30,10 @@ if (!(!empty($_GET['user']) && !empty($_GET['recoverpass']))) {
                     //Set who the message is to be sent to
                     $mail->addAddress($user->getEmail());
                     //Set the subject line
-                    $mail->Subject = 'Reset Your ' . $config->getWebSiteTitle() . ' Password';
-                    $msg = '<img class="max-width" border="0" style="display:block; color:#000000; text-decoration:none; font-family:Helvetica, arial, sans-serif; font-size:16px; max-width:40% !important; width:40%; height:auto !important;" width="240" alt="" data-proportionally-constrained="true" data-responsive="true" src="http://cdn.mcauto-images-production.sendgrid.net/7dc20d38122c638b/af3800ee-09d2-4c17-b2ef-01da8ba7a0b5/1920x1080.png">';
-                    $msg .= '<h2 style="text-align: inherit"><span style="overflow-wrap: break-word">' . $mail->Subject . '</span>&nbsp;</h2><br><br>'
-                    $msg .= __("You asked for a recover link, click on the provided link", $config->getWebSiteTitle(), $config->getWebSiteTitle()) . "<br><br><div><a href='{$global['webSiteRootURL']}recoverPass?user={$_POST['user']}&recoverpass={$recoverPass}' style='margin: 0 auto; background-color:#3A9BDC; border:1px solid #333333; border-color:#333333; border-radius:6px; border-width:1px; color:#ffffff; display:inline-block; font-size:14px; font-weight:normal; letter-spacing:0px; line-height:normal; padding:12px 18px 12px 18px; text-align:center; text-decoration:none; border-style:solid;' target='_blank'>" . __("Reset password") . "</a></div>";
-                    $msg .= "If the button does not work, paste this URL into your browser: {$global['webSiteRootURL']}recoverPass?user={$_POST['user']}&recoverpass={$recoverPass}";
+                    $mail->Subject = 'Recover Pass from ' . $config->getWebSiteTitle();
+
+                    $msg = __("You asked for a recover link, click on the provided link") . " <a href='{$global['webSiteRootURL']}recoverPass?user={$_POST['user']}&recoverpass={$recoverPass}'>" . __("Reset password") . "</a>";
+
                     $mail->msgHTML($msg);
 
                     //send the message, check for errors
